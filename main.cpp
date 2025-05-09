a5b02e7abb8dd6ed797af896992c71f090455114
PurpleGym01 <kubikmira007@gmail.com>
2025-05-10 00:04:28 +0300

    pole

--- main.cpp
+++ main.cpp
@@ -9,6 +9,22 @@ int main() {
     now = levels[selected_level];
 
     sf::RenderWindow playw(sf::VideoMode({ 900, 600 }), "Sudoku");
+    playw.clear(sf::Color::White);
+
+    sf::RectangleShape cell(sf::Vector2f(450.f, 450.f));
+    cell.setPosition({ 50, 50 });
+    cell.setFillColor(sf::Color::Black);
+    playw.draw(cell);
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            sf::RectangleShape cell1(sf::Vector2f(48.f, 48.f));
+            float x = 51 + i * 50, y = 51 + j * 50;
+            cell1.setPosition(sf::Vector2f(x, y));
+            cell1.setFillColor(sf::Color::White);
+            playw.draw(cell1);
+        }
+    }
+    playw.display();
 
     while (playw.isOpen()) {
 
@@ -17,6 +33,7 @@ int main() {
             // Close window: exit
             if (event->is<sf::Event::Closed>())
                 playw.close();
+
         }
 
     }
